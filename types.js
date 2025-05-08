0ebc0dce7278350f80263d034d625191844f5654
Michael Cortella <mcortella@cnm.edu>
2025-05-07 19:50:38 -0600

    Variables data and types

---
+++ types.js
@@ -0,0 +1,18 @@
+/*
+ * Variables, Data Types, and Typing
+ */
+let variable : string = "string";
+
+let var1 = "2";
+let var2 = 5;
+let result = var1 + var2;
+
+let add = 1+ 2;
+let sub = 2 - 1;
+let mult = 2 * 4;
+let div = 4 / 2;
+let mod = 5 % 2;
+
+let string = "String 1 " + "String 2"
+
+console.log(result);
---
+++ varables-and-data.js
@@ -0,0 +1,12 @@
+/*
+ * Variables, Data Types, and Typing
+ */
+Let wordData;
+
+wordData = "Word List";
+
+console.log(Word Data);
+
+let subTotal = 100;
+let tax = 0.07;
+let totalWithTax = tax + subTotal;
